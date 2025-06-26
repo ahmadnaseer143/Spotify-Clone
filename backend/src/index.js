20e42948b7a2952adc7f596cbd8809a6822db3c6
ahmadnaseer143 <anaseer942@gmail.com>
2025-06-26 14:43:41 -0500

    protect routes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import { clerkMiddleware } from "@clerk/express";
 
 import { connectToDB } from "./lib/db.js";
 
@@ -15,6 +16,8 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+
+app.use(clerkMiddleware());
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
