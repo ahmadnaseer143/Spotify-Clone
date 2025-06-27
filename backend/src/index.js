49ded75a81c711b95771002d14984b10ccdbac36
ahmadnaseer143 <anaseer942@gmail.com>
2025-06-27 13:23:10 -0500

    cloudinary and fileupload set

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
 import { clerkMiddleware } from "@clerk/express";
+import fileUpload from "express-fileupload";
+import path from "path";
 
 import { connectToDB } from "./lib/db.js";
 
@@ -12,12 +14,23 @@ import albumRoutes from "./routes/albums.route.js";
 import statRoutes from "./routes/stats.route.js";
 dotenv.config();
 
+const __dirname = path.resolve();
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
 
 app.use(clerkMiddleware());
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: path.join(__dirname, "temp"),
+    createParentPath: true,
+    limits: {
+      fileSize: 10 * 1024 * 1024,
+    },
+  })
+);
 
 app.use("/api/users", userRoutes);
 app.use("/api/admin", adminRoutes);
