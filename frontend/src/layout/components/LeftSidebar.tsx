55065733fdde1c5ffe3acf2b5570ad34c0bf1aff
ahmadnaseer143 <anaseer942@gmail.com>
2025-07-03 12:38:34 -0500

    navigation left sidebar

---
+++ frontend/src/layout/components/LeftSidebar.tsx
@@ -0,0 +1,47 @@
+import { buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { SignedIn } from "@clerk/clerk-react";
+import { Link } from "react-router-dom";
+import { HomeIcon, MessageCircle } from "lucide-react";
+
+const LeftSidebar = () => {
+  return (
+    <div className="h-full flex flex-col gap-2">
+      {/* Navigation menu */}
+
+      <div className="rounded-lg bg-zinc-900 p-4">
+        <div className="space-y-2">
+          <Link
+            to={"/"}
+            className={cn(
+              buttonVariants({
+                variant: "ghost",
+                className: "w-full justify-start text-white hover:bg-zinc-800",
+              })
+            )}
+          >
+            <HomeIcon className="mr-2 size-5" />
+            <span className="hidden md:inline">Home</span>
+          </Link>
+
+          <SignedIn>
+            <Link
+              to={"/chat"}
+              className={cn(
+                buttonVariants({
+                  variant: "ghost",
+                  className:
+                    "w-full justify-start text-white hover:bg-zinc-800",
+                })
+              )}
+            >
+              <MessageCircle className="mr-2 size-5" />
+              <span className="hidden md:inline">Messages</span>
+            </Link>
+          </SignedIn>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default LeftSidebar;
